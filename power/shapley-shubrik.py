69ecb967270f5aaf3b8bd8b553476296e5c7f638
MurphyPOne <MurphyP1@bishopireton.org>
2022-03-30 22:04:26 -0400

    fixed Banzhaf

--- power/shapley-shubrik.py
+++ power/shapley-shubrik.py
@@ -1,6 +1,7 @@
 from typing import List
-from collections import defaultdict
-from itertools import permutations
+from itertools import permutations, combinations
+import matplotlib.pyplot as plt
+
 
 class Coalition():
     def __init__(self, majority: int, weights: List[int]):
@@ -11,7 +12,31 @@ class Coalition():
         self.weights = {f"P{i+1}": w for i, w in enumerate(weights)}
 
     def get_perms(self):
+        """gets all (comprehensive) permutations of weights"""
         return list(permutations(self.weights))
+
+    def get_combos(self):
+        """gets all (partial) combinations of weights"""
+        def flatten(lls):
+            flattened = []
+            for e in lls:
+                if type(e) is list:
+                    for item in e:
+                        flattened.append(item)
+                else:
+                    flattened.append(e)
+            return flattened
+
+        combos = []
+        for r in range(len(self.weights) + 1):
+            combos.append(list(combinations(self.weights, r)))
+        return flatten(list(combos))
+
+    def sum_perm(self, perm):
+        """given a tuple of the form ('P1', 'P2', 'P3') return the sum of the association weights"""
+
+        return sum(self.weights[p] for p in perm)
+    
 
     def count_pivotal(self):
         """Shapley-Shubrik"""
@@ -30,39 +55,41 @@ class Coalition():
                     break 
         
         n_perms = len(perms)
-        res = {k: round(float(v)/n_perms, 4) for k, v in res.items()}
+        for k, v in res.items():
+            if n_perms > 0:
+                res[k] = round(float(v)/n_perms, 4)
+            else: 
+                res[k] = 0
+        # res = {k: round(float(v)/n_perms, 4) for k, v in res.items()}
         return res
-
-    def sum_perm(self, perm):
-        """given a tuple of the form ('P1', 'P2', 'P3') return the sum of the association weights"""
-
-        return sum(self.weights[p] for p in perm)
-
 
     def count_critical(self):
         """Banzhaf"""
         # start by listing all coalitions, then eliminating non winning coalitions
-        perms = self.get_perms()
-        winning_perms = [ perm for perm in perms if self.sum_perm(perm) > self.majority ]
+        combos = self.get_combos()
+        winning_combos = [ combo for combo in combos if self.sum_perm(combo) >= self.majority ]
         critical_counts = { k: 0 for k in self.weights }
         
         # for each passing coalition
-        for wp in winning_perms:
-            total = 0
+        for wp in winning_combos:
+            total = self.sum_perm(wp)
 
             # iterate over each voter 
             for p in wp:
                 # if their cast vote meets/beats the majority, break
-                if total >= self.majority:
-                    print()
-                    break 
+                if total - self.weights[p] < self.majority:
+                    critical_counts[p] += 1
+                # print(p, total)
+        
+        n_critical = sum([critical_counts[k] for k in critical_counts])
+        # critical_counts = {k: round(float(v)/n_critical, 4) for k, v in critical_counts.items()}
+        for k, v in critical_counts.items():
+            if n_critical > 0:
+                critical_counts[k] = round(float(v)/n_critical, 4)
+            else: 
+                critical_counts[k] = 0
 
-                total += self.weights[p]
-                critical_counts[p] += 1
-                print(p, total)
-                
         return critical_counts
-
 
     def __repr__(self):
         dict_str = ""
@@ -71,25 +98,38 @@ class Coalition():
         dict_str = dict_str[:-2]
 
         res = f"[{self.majority}: {dict_str}".replace("[", "").replace("]", "")
-        return f"[{res}]"
+        res = f"[{res}]"
+        res += f"\nShapley-Shubrik:\t{self.count_pivotal()}"
+        res += f"\nBanzhaf:\t\t{self.count_critical()}\n"
+        return res
+
+## Tests
 
 C1 = Coalition(6, [4, 3, 2])
 print(C1)
-# print(C1.get_perms())
-print(C1.count_pivotal())
 
 C2 = Coalition(36, [20, 17, 15])
 print(C2)
-# print(C2.get_perms())
-print(C2.count_pivotal())
 
 C3 = Coalition(8, [6, 4, 3, 2])
 print(C3)
-# print(C3.get_perms())
-print(C3.count_pivotal())
-
-print()
 
 C4 = Coalition(8, [6, 3, 2])
 print(C4)
-print(C4.count_critical())
+
+# make an EC map by weight
+# TODO: modify the constructor to accept labels
+C5 = Coalition(270, [55, 38, 16])
+print(C5)
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+labels, weights = C5.count_critical().keys(), C5.count_critical().values()
+ax1.pie(weights, labels=labels, autopct='%1.1f%%')
+ax1.set_title("Shapley-Shubrik")
+
+labels, weights = C5.count_pivotal().keys(), C5.count_pivotal().values()
+ax2.pie(weights, labels=labels, autopct='%1.1f%%')
+ax2.set_title("Banzhaf")
+
+plt.show()
